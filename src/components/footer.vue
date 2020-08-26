adb45ede8dd5339451f1b62b17f7eeffed1199dc
yoshihide <aqua.sky.33333@gmail.com>
2020-08-26 20:47:17 +0900

    qiita追加

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -8,6 +8,11 @@
           </a>
         </li>
         <li>
+          <a href="https://qiita.com/yoshihide_tsukamoto" target="_blank">
+            <img src="../assets/images/qiita.png" alt />
+          </a>
+        </li>
+        <li>
           <a href="https://github.com/yoshihide1/" target="_blank">
             <img src="../assets/images/github-icon-2.svg" alt />
           </a>
@@ -30,7 +35,7 @@ export default {};
 <style scoped>
 .footer__sns {
   border-top: 2px solid #bac8c6;
-    background-color: #494544;
+  background-color: #494544;
   padding-bottom: 0.5rem;
 }
 .sns__items {
@@ -46,15 +51,14 @@ export default {};
 }
 .copyright {
   color: #bac8c6;
-    border-bottom: 2px solid #c41a30;
-    padding: 0 1rem;
-    margin-bottom: 0.5rem;
-
+  border-bottom: 2px solid #c41a30;
+  padding: 0 1rem;
+  margin-bottom: 0.5rem;
 }
 @media screen and (max-width: 769px) {
- #footer {
-   height: 10vh;
-   padding-bottom: 0.5rem;
- } 
+  #footer {
+    height: 10vh;
+    padding-bottom: 0.5rem;
+  }
 }
 </style>
